029cce641079a1a199565658fdcee811e89d8ca0
Kuze-Dior <sarallorenzo6@gmail.com>
2025-02-09 18:24:38 +0800

    Saral_Calculator.java

--- Saral_Calculator.java
+++ Saral_Calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Vicentino_Calculator {
+public class Saral_Calculator {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("enter your first numebr here ");
