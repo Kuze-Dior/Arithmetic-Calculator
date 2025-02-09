2f320dad70cccb4d6e5b76643da406f6b31ce480
Kuze-Dior <sarallorenzo6@gmail.com>
2025-02-09 18:58:01 +0800

    Add files via upload

---
+++ LorenzoSaralII_MyFirstApp.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+ public class LorenzoSaralIII {
+    public static void main(String[] args) {
+    Date date = new Date();
+    System.err.println("Lorenzo D. Saral III");
+    System.out.println(date);
+    }
+}
